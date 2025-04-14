4e8503dc41a8372d2dde1919eec834bdb484927f
Julio Munoz <julioc.munozg@gmail.com>
2025-04-13 20:49:27 -0400

    Second Commit

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -13,22 +13,32 @@ def emotion_detector(text_to_analyze):
 
     # Make a POST request to the API with the payload and headers
     response = requests.post(url, json=myobj, headers=header)
+    status_code = response.status_code
 
-    # Parse the response from the API
-    formatted_response = json.loads(response.text)
+    if status_code == 200:
+        # Parse the response from the API
+        formatted_response = json.loads(response.text)
+        # Extract emotion scores
+        emotion_scores = formatted_response['emotionPredictions'][0]['emotion']
 
-    # Extract emotion scores
-    emotion_scores = formatted_response['emotionPredictions'][0]['emotion']
-
-    # Find the dominant emotion
-    dominant_emotion = max(emotion_scores, key=emotion_scores.get)
-
-    # Return the dictionary with scores and dominant emotion
-    return {
-        'anger': emotion_scores['anger'],
-        'disgust': emotion_scores['disgust'],
-        'fear': emotion_scores['fear'],
-        'joy': emotion_scores['joy'],
-        'sadness': emotion_scores['sadness'],
-        'dominant_emotion': dominant_emotion
-    }
+        # Find the dominant emotion
+        dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+    
+        # Return the dictionary with scores and dominant emotion
+        return {
+            'anger': emotion_scores['anger'],
+            'disgust': emotion_scores['disgust'],
+            'fear': emotion_scores['fear'],
+            'joy': emotion_scores['joy'],
+            'sadness': emotion_scores['sadness'],
+            'dominant_emotion': dominant_emotion
+        }
+    elif status_code == 400:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
